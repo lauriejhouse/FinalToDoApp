a749e73c24bd03b90360377b124e6e3be4d527b0
Unknown <jackienorstrom@aol.com>
2019-02-15 14:20:18 -0500

    working on adding delete function back in.

--- ToDoFinalApp/CoreDataManager/CloudKitManager.swift
+++ ToDoFinalApp/CoreDataManager/CloudKitManager.swift
@@ -203,6 +203,59 @@ class CloudKitManager {
     }
     
     
+     func deleteRecordsFromCloud( list : [CKRecord], completion: @escaping (Bool) -> Void ) {
+        
+        if list.count == 0 {
+            DispatchQueue.main.async {
+                completion(true)
+            }
+            return;
+        }
+        
+        DispatchQueue.global(qos: .background).async {
+            
+            let count = min(400, list.count)
+            
+            var newList = list
+            var records : [CKRecord.ID] = []
+            
+            for _ in 0..<count {
+                let object = newList[0]
+                let objectId = object.recordID
+                records.append(objectId)
+                newList.remove(at: 0)
+            }
+            
+            let deleteRecords = CKModifyRecordsOperation.init(recordsToSave: nil, recordIDsToDelete: records)
+            deleteRecords.modifyRecordsCompletionBlock = ({(savedRecords, deletedRecords, operationError) -> Void in
+                if let error = operationError {
+                    print("deleteRecordsFromCloud, deleteRecords error:",error)
+                } else {
+                    Cloud.deleteRecordsFromCloud(list: newList, completion: completion)
+                }
+            })
+            
+            self.publicDb.add(deleteRecords)
+        }
+    }
+    
+//     func deleteItemFromCloud( _ item: NSManagedObject ) {
+//        
+//        let recordId = Cloud.recordIdForItem(item)
+//        
+//        privateDb.fetch(withRecordID: recordId) { record, error in
+//            if let record = record {
+//                Cloud.deleteRecordsFromCloud(list: [record] ) { completion in
+//                    print("Deleted item from Cloud", item.description)
+//                }
+//            } else {
+//                print("Item not found on Cloud", item.description)
+//            }
+//        }
+//    }
+    
+    
+    
     func getAllTasksForToday( completion : @escaping (([Task]) -> Void) ) {
         getAllGoals() { goals in
             var tasks = [Task]()
