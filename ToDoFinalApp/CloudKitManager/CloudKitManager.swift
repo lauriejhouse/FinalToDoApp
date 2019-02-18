5c1c4b1eeecaa0a7aa6884a37c9d91d4c22117f0
Unknown <jackienorstrom@aol.com>
2019-02-18 08:20:33 -0500

    Need to get delete function working.

--- ToDoFinalApp/CloudKitManager/CloudKitManager.swift
+++ ToDoFinalApp/CloudKitManager/CloudKitManager.swift
@@ -204,39 +204,39 @@ class CloudKitManager {
     }
     
     
-    func deleteTask(with task: Task) {
-        publicDb.fetch(withRecordID: task.recordId) { record, error in
-            if let record = record {
-                record.setValue(task.taskName, forKey: "taskName")
-                record.setValue(task.completed, forKey: "completed")
-                record.setValue(task.dueDate, forKey: "dueDate")
-                
-//                let operation = CKModifyRecordsOperation(recordsToSave: nil, recordIDsToDelete: [record])
-                let operation = CKModifyRecordsOperation.init(recordsToSave: nil, recordIDsToDelete: [record])
-                
-//                operation.modifyRecordsCompletionBlock = { savedRecords, deletedRecordIDs, error in
-//                    if let error = error {
-//                        print("There was an error: \(error.localizedDescription)")
-//                        return
-//                    }
-//                }
-                operation.modifyRecordsCompletionBlock = ({(savedRecords, deletedRecords, operationError) -> Void in
-                        if let error = operationError {
-                        print("deleteRecordsFromCloud, deleteRecords error:",error)
-                        } else {
-                    publicDb.deleteRecordsFromCloud(list: newList, completion: completion)
-                        }
-                        })
-                
-        
-                self.publicDb.add(operation)
-                
-            } else {
-//                print("Goal not found on Cloud", goal.goalName)
-            }
-            
-        }
-    }
+//    func deleteTask(with task: Task) {
+//        publicDb.fetch(withRecordID: task.recordId) { record, error in
+////            if let record = record {
+////                record.setValue(task.taskName, forKey: "taskName")
+////                record.setValue(task.completed, forKey: "completed")
+////                record.setValue(task.dueDate, forKey: "dueDate")
+//            
+//
+//                let operation = CKModifyRecordsOperation.init(recordsToSave: nil, recordIDsToDelete: [record])
+//                
+////                operation.modifyRecordsCompletionBlock = { savedRecords, deletedRecordIDs, error in
+////                    if let error = error {
+////                        print("There was an error: \(error.localizedDescription)")
+////                        return
+////                    }
+////                }
+//                operation.modifyRecordsCompletionBlock = ({(savedRecords, deletedRecords, operationError) -> Void in
+//                        if let error = operationError {
+//                        print("deleteRecordsFromCloud, deleteRecords error:",error)
+//                        } else {
+//                    publicDb.deleteRecordsFromCloud(list: newList, completion: completion)
+//                        }
+//                        })
+//                
+//        
+//                self.publicDb.add(operation)
+//                
+//            } else {
+////                print("Goal not found on Cloud", goal.goalName)
+//            }
+//            
+//        }
+//    }
     
     
    //From louis example
@@ -348,3 +348,67 @@ class CloudKitManager {
     }
 */
 }
+
+
+
+/* Louie Example delete stuff from cloud.
+ //delete both entry and data set, delete row on table, and on cloudkit.
+ /*
+ 
+ class func deleteRecordsFromCloud( list : [CKRecord], completion: @escaping (Bool) -> Void ) {
+ 
+ if list.count == 0 {
+ DispatchQueue.main.async {
+ completion(true)
+ }
+ return;
+ }
+ 
+ DispatchQueue.global(qos: .background).async {
+ 
+ let count = min(400, list.count)
+ 
+ var newList = list
+ var records : [CKRecordID] = []
+ 
+ for _ in 0..<count {
+ let object = newList[0]
+ let objectId = object.recordID
+ records.append(objectId)
+ newList.remove(at: 0)
+ }
+ 
+ let deleteRecords = CKModifyRecordsOperation.init(recordsToSave: nil, recordIDsToDelete: records)
+ deleteRecords.modifyRecordsCompletionBlock = ({(savedRecords, deletedRecords, operationError) -> Void in
+ if let error = operationError {
+ print("deleteRecordsFromCloud, deleteRecords error:",error)
+ } else {
+ Cloud.deleteRecordsFromCloud(list: newList, completion: completion)
+ }
+ })
+ 
+ privateDb.add(deleteRecords)
+ }
+ }
+ 
+ class func deleteItemFromCloud( _ item: NSManagedObject ) {
+ 
+ let recordId = Cloud.recordIdForItem(item)
+ 
+ privateDb.fetch(withRecordID: recordId) { record, error in
+ if let record = record {
+ Cloud.deleteRecordsFromCloud(list: [record] ) { completion in
+ print("Deleted item from Cloud", item.description)
+ }
+ } else {
+ print("Item not found on Cloud", item.description)
+ }
+ }
+ }
+ 
+ 
+ 
+ 
+ */
+ 
+ */
